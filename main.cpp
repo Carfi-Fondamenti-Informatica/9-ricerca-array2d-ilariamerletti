546871ec8bbe0b00fc5a0aea63c4ab780ff3d43e
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-05-14 18:46:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@ using namespace std;
 int main() {
     char mat[10][20];
     char nome[20];
-    cout << "inserire 10 nomi"<< endl;
     inserimento(mat);
-    cout << "inserire nome da cercare"<< endl;
     inserimento(nome);
     cout << endl;
-    cout << "il nome si trova in posizione:  ";
     cout<< confronto(mat,nome);
     return 0;
 }
