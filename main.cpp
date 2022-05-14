f15fd013b2b6cd8c6d56c4ee8177c0450174de64
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-05-14 18:43:39 +0200

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
-int main(){
-  return 0;
+using namespace std;
+#include "lib.h";
+int main() {
+    char mat[10][20];
+    char nome[20];
+    cout << "inserire 10 nomi"<< endl;
+    inserimento(mat);
+    cout << "inserire nome da cercare"<< endl;
+    inserimento(nome);
+    cout << endl;
+    cout << "il nome si trova in posizione:  ";
+    cout<< confronto(mat,nome);
+    return 0;
 }
